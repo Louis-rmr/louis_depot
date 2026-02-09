478365c5b4ff7c98155275bc7e3d396a7fec0518
louis-rmr <lromero@etu.isara.fr>
2026-02-09 12:15:05 +0100

    affichage de la saison choisie en tableau

--- app_test_louis/server.R
+++ app_test_louis/server.R
@@ -5,5 +5,9 @@ got <- read.table("data/got.csv", header=TRUE, sep=";", dec=",")
 
 function(output, input){
   
-  output$courbe <- renderTable(got)}
+  output$courbe <- renderTable({
+    got%>%
+      filter(season==input$saison)
+  })
+}
     
